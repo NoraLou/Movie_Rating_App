8fd1d0f03490859a0ce0bd9211935eecd6f42e13
Hackbright Student <info@hackbrightacademy.com>
2014-10-23 13:05:10 -0700

    Started session/cursor stuff

--- model.py
+++ model.py
@@ -1,10 +1,47 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movies(Base):
+    __tablename__ = 'movies'
+
+    movie_id = Column(Integer, primary_key = True)
+    movie_title = Column(String(64))
+    release_date = Column(Integer)
+
+class Ratings(Base):
+    __tablename__ = 'ratings'
+
+    user_id = Column(Integer, primary_key = True)
+    item_id = Column(Integer, ForeignKey('movies.movie_id'))
+    rating = Column(Integer, nullable=True)
+    timestamp = Column(Integer)
+
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 
 ### End class declarations
@@ -12,6 +49,7 @@ Base = declarative_base()
 def main():
     """In case we need this for something"""
     pass
+
 
 if __name__ == "__main__":
     main()
--- seed.py
+++ seed.py
@@ -3,19 +3,31 @@ import csv
 
 def load_users(session):
     # use u.user
+     # user id | age O | gender 0| occupation 0 | zip code 0| username | password 
+     # The user ids are the ones used in the u.data data set.
     pass
 
 def load_movies(session):
     # use u.item
+    #       movie id | movie title | release date | video release date |
+
+
+        # 1 indicates the movie
+        #       is of that genre, a 0 indicates it is not; movies can be in
+        #       several genres at once.
+        #       The movie ids are the ones used in the u.data data set.
+
     pass
 
 def load_ratings(session):
     # use u.data
+    # u.data columns user id | item id | rating | timestam
     pass
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     pass
+    
 
 if __name__ == "__main__":
     s= model.connect()
